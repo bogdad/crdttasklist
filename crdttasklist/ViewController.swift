ed17c9263b20cb22d58883bffc93788e2022c8ca
Vladimir Shakhov <vladimir@spotify.com>
2019-05-24 14:59:35 +0200

    Initial Commit

---
+++ crdttasklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  crdttasklist
+//
+//  Created by Vladimir Shakhov on 5/24/19.
+//  Copyright © 2019 Vladimir Shakhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
