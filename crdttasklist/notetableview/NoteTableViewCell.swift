06ed6d3fd8d30da338cf825bb0c9ea6bf5a2ba09
Vladimir Shakhov <bogdad@gmail.com>
2020-06-19 14:23:57 +0200

    small intensity for checklist tasks by default

--- crdttasklist/notetableview/NoteTableViewCell.swift
+++ crdttasklist/notetableview/NoteTableViewCell.swift
@@ -26,11 +26,11 @@ class NoteTableViewCell: UITableViewCell {
         let int1 = note.intensity1()
         let int2 = note.intensity2()
         if int1 > 0 && int2 > 0 {
-            backgroundColor = UIColor.systemRed.withAlphaComponent(CGFloat((int1 + int2) * base))
+            backgroundColor = UIColor.systemRed.withAlphaComponent(CGFloat((0.1 + int1 + int2) * base))
         } else if int1 > 0 {
-            backgroundColor = UIColor.systemPink.withAlphaComponent(CGFloat(int1 * base))
+            backgroundColor = UIColor.systemPink.withAlphaComponent(CGFloat((0.1 + int1) * base))
         } else if int2 > 0 {
-            backgroundColor = UIColor.systemOrange.withAlphaComponent(CGFloat(int2 * base))
+            backgroundColor = UIColor.systemOrange.withAlphaComponent(CGFloat((0.1 + int2) * base))
         } else {
             backgroundColor = Design.grayBackround()
         }
